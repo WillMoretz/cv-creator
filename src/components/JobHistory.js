17b141eae52565ffe9ba2393a737af2d06426b83
Will Moretz <whmoretz@gmail.com>
2023-04-07 16:32:01 -0400

    added add button functionality

--- src/components/JobHistory.js
+++ src/components/JobHistory.js
@@ -1,12 +1,36 @@
+/* eslint-disable no-useless-constructor */
 import { Component } from "react";
 import JobHistoryItem from "./JobHistoryItem";
+import { v4 as uuid } from "uuid";
 
 export default class JobHistory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  reset() {
+    this.setState((prevState) => ({
+      items: [],
+    }));
+  }
+
+  addItem() {
+    this.setState((prevState) => ({
+      items: [...prevState.items, <JobHistoryItem key={uuid()} />],
+    }));
+  }
+
   render() {
+    const jobHistoryItems = this.state.items.map((item) => item);
+
     return (
       <div>
-        <JobHistoryItem />
-        <button>Add</button>
+        <div>{jobHistoryItems}</div>
+        <button onClick={() => this.addItem()}>Add</button>
       </div>
     );
   }
