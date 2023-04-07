090c10e5aef7d54ea02762c0404fbdc742c09f2b
Will Moretz <whmoretz@gmail.com>
2023-04-07 10:40:57 -0400

    linked components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { Component } from "react";
+import Education from "./components/Education";
+import JobHistory from "./components/JobHistory";
+import PersonalInfo from "./components/PersonalInfo";
 
 export default class App extends Component {
   render() {
-    return <div className="App">Hello React</div>;
+    return (
+      <div className="App">
+        <header>Cover Letter Creator</header>
+        <PersonalInfo />
+        <JobHistory />
+        <Education />
+        <footer>Made by Will Moretz</footer>
+      </div>
+    );
   }
 }
