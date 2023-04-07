f8576ddc648ec531e0ed07a2d8ade254b346ced2
Will Moretz <whmoretz@gmail.com>
2023-04-07 11:08:34 -0400

    created job history and education forms

--- src/components/EducationItem.js
+++ src/components/EducationItem.js
@@ -2,6 +2,38 @@ import { Component } from "react";
 
 export default class EducationItem extends Component {
   render() {
-    return <div>Education Item</div>;
+    return (
+      <form>
+        <div className="form-row">
+          <label htmlFor="school-name">School Name: </label>
+          <input type="text" name="school-name" id="school-name" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="subject">Subject: </label>
+          <input type="text" name="subject" id="subject" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="degree">Degree: </label>
+          <input type="text" name="degree" id="degree" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="education-start-date">Start: </label>
+          <input
+            type="text"
+            name="education-start-date"
+            id="education-start-date"
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="education-end-date">End: </label>
+          <input
+            type="text"
+            name="education-end-date"
+            id="education-end-date"
+          />
+        </div>
+        <button type="button">Delete</button>
+      </form>
+    );
   }
 }
--- src/components/JobHistoryItem.js
+++ src/components/JobHistoryItem.js
@@ -2,6 +2,26 @@ import { Component } from "react";
 
 export default class JobHistoryItem extends Component {
   render() {
-    return <div>Job History Item</div>;
+    return (
+      <form>
+        <div className="form-row">
+          <label htmlFor="position">Position: </label>
+          <input type="text" name="position" id="position" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="company">Company: </label>
+          <input type="text" name="company" id="company" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="job-start-date">Start: </label>
+          <input type="text" name="job-start-date" id="job-start-date" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="job-end-date">End: </label>
+          <input type="text" name="job-end-date" id="job-end-date" />
+        </div>
+        <button type="button">Delete</button>
+      </form>
+    );
   }
 }
