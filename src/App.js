c48fd2deac189c6f38263d1800d146cb5fdb7d11
Will Moretz <whmoretz@gmail.com>
2023-04-08 11:40:46 -0400

    created cover letter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-useless-constructor */
 import { Component } from "react";
+import CoverLetter from "./components/CoverLetter";
 import Education from "./components/Education";
 import JobHistory from "./components/JobHistory";
 import PersonalInfo from "./components/PersonalInfo";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +17,7 @@ export default class App extends Component {
         <PersonalInfo />
         <JobHistory />
         <Education />
+        <CoverLetter />
         <footer>Made by Will Moretz</footer>
       </div>
     );
---
+++ src/components/CoverLetter.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class CoverLetter extends Component {
+  render() {
+    return <div>Cover Letter</div>;
+  }
+}
